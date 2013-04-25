c145d1145d42840fd8a0084df6a273a7f7cc6af5
kevin <ktripp1@gmail.com>
2013-04-24 23:41:58 -0400

    initializing the homework for class 25.

---
+++ Sites/my_book_review/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBookReview::Application.config.secret_token = '99c63190050ad0f651b53f33bef7d3579f126f3cd630ad5a06a6107a55c0c9569456272bce96e0df85511f38ab8e72b96281a51f8a2a2adaac4804d1175e75ae'
