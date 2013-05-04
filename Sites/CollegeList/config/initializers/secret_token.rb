1654b46a555d302fb563c48db4c665c3ff230015
kevin <ktripp1@gmail.com>
2013-05-04 15:56:18 -0400

    another commit

---
+++ Sites/CollegeList/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollegeList::Application.config.secret_token = '56f2eed397783b971e2ba0300571bc142294be46bb7eb6f67e58ba87ed5d06ddb6ac4caa24c7ae08b8a28c291c04b28f3db4a613fb4560ca7622f621fb461d96'
